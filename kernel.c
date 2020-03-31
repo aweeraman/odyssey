403f41d9cd9d9d763a51ee4034f8d3ea267d13a9
Anuradha Weeraman <aweeraman@gmail.com>
2020-03-31 08:41:51 -0400

    Spinner

--- kernel.c
+++ kernel.c
@@ -1,18 +1,25 @@
 #define ROWS 25
 #define COLS 80
 
-void kernel_main(void) {
+void pause(void) {
+  for (long l=0L; l<1000000; l++);
+}
 
+void kernel_main(void) {
   volatile unsigned char *terminal_buf = (volatile unsigned char *) 0xb8000;
-  const char *str = "oh hai";
 
   for (int i = 0; i < (ROWS * COLS); i += 2) {
     terminal_buf[i] = 0;
     terminal_buf[i+1] = 15;
   }
 
-  for (int i = 0, j = 0; str[i] != '\0'; i++, j += 2) {
-    terminal_buf[j] = str[i];
-    terminal_buf[j+1] = 15;
+  while (1) {
+    terminal_buf[0] = '|';
+    pause();
+    terminal_buf[0] = '/';
+    pause();
+    terminal_buf[0] = '-';
+    pause();
+    terminal_buf[0] = '\\';
   }
 }
