86d13d371ff463e2a1d8fc4a6a0a3830da752f0d
Anuradha Weeraman <aweeraman@gmail.com>
2020-03-31 19:45:11 -0400

    Multiboot

--- boot.s
+++ boot.s
@@ -1,18 +1,21 @@
-  .set STACK_SIZE, 4096
+  .set STACK_SIZE,    4096
+  .set MB2_MAGIC,     0xe85250d6
+  .set MB2_ARCH_FLAG, 0x0
+  .set MB2_FB_TAG,    0x0
 
   .global _start
 
   .section .text
-multiboot_start:
+mb2_hdr:
   .align 4
-  .long 0xe85250d6
-  .long 0x0
-  .long multiboot_end - multiboot_start
-  .long 0x100000000 - (0xe85250d6 + 0 + (multiboot_end - multiboot_start))
-  .long 0x0
-  .long 0x0
-  .long 0x8
-multiboot_end:
+  .long MB2_MAGIC             # multiboot2 magic string
+  .long MB2_ARCH_FLAG         # architecture
+  .long mb2_hdr_end - mb2_hdr # header length
+  .long -(MB2_MAGIC + MB2_ARCH_FLAG + (mb2_hdr_end - mb2_hdr)) # checksum
+  .word 0x0                   # end tag type
+  .word 0x0                   # end tag flags
+  .long 0x8                   # size, 8 to terminate
+mb2_hdr_end:
 
 _start:
   mov $stack_top, %esp
