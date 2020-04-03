64b221c9cfaf27e3616386181f1158c6777591fb
Anuradha Weeraman <aweeraman@gmail.com>
2020-04-03 17:29:10 -0400

    Multiboot

--- boot.s
+++ boot.s
@@ -10,9 +10,12 @@ mb2_hdr:
   .long MB2_ARCH_FLAG         # architecture
   .long mb2_hdr_end - mb2_hdr # header length
   .long -(MB2_MAGIC + MB2_ARCH_FLAG + (mb2_hdr_end - mb2_hdr)) # checksum
-  .word 0x0                   # end tag type
-  .word 0x0                   # end tag flags
-  .long 0x8                   # size, 8 to terminate
+  .align 8
+tag_end:
+  .word 0                   # end tag type
+  .word 0                   # end tag flags
+  .long 8                   # size, 8 to terminate
+tag_end_end:
 mb2_hdr_end:
 
   .section .text
