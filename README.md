196d9d727c7fc022ab36f47bfb4582f5342c94a2
Anuradha Weeraman <aweeraman@gmail.com>
2020-04-30 22:05:21 -0400

    Externalized segment offsets

--- README.md
+++ README.md
@@ -135,6 +135,7 @@ Minos makes use of following software:
 
 [OSDev Wiki](https://wiki.osdev.org/Main_Page) is an invaluable resource
 for hobbyist operating systems development.
+[Intel® 64 and IA-32 Architectures Software Developer Manuals](https://software.intel.com/en-us/articles/intel-sdm)
 
 # License
 
--- src/load_gdt.asm
+++ src/load_gdt.asm
@@ -15,20 +15,23 @@
 ; Copyright 2020, Anuradha Weeraman
 ;
 
+%define KERNEL_CODE_SEGMENT 0x08
+%define KERNEL_DATA_SEGMENT 0x10
+
 global load_gdt
 
 load_gdt:
   mov eax, [esp + 4]
   lgdt [eax]
 
-  mov ax, 0x10
+  mov ax, KERNEL_DATA_SEGMENT
   mov ds, ax
   mov es, ax
   mov fs, ax
   mov gs, ax
   mov ss, ax
 
-  jmp 0x08:reload
+  jmp KERNEL_CODE_SEGMENT:reload
   ret
 
 reload:
