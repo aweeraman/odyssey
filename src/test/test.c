8fa8d76d8d8a16410c60caec1f9115d598e6f57e
Anuradha Weeraman <aweeraman@gmail.com>
2020-05-14 21:06:56 -0400

    Moved mm tests out of test.c

--- src/test/test.c
+++ src/test/test.c
@@ -7,10 +7,6 @@
 #include <lib/termio.h>
 #include <sys/memory.h>
 
-#ifdef CONFIG_MM_FF
-#include <sys/ffmm.h>
-#endif
-
 static int tests_passed;
 static int tests_failed;
 
@@ -18,28 +14,6 @@ void fail(const char *test, const int line, char *msg)
 {
         printf("Test %s failed: line %d, %s\n", test, line, msg);
 }
-
-#ifdef CONFIG_MM_FF
-static int ff_mm_1()
-{
-        int ret = 0;
-        char* frame;
-
-        frame = get_free_frame(MEM_FRAME_SIZE+1);
-        ASSERT(frame != NULL, "allocate frame larger than the max frame size");
-
-        frame = get_free_frame(MEM_FRAME_SIZE-1);
-        memset(frame, 'a', MEM_FRAME_SIZE-1);
-        strncpy(frame, "123", MEM_FRAME_SIZE-1);
-        ASSERT(!(frame[0] == '1' && frame[1] == '2' &&
-                 frame[2] == '3' && frame[3] == '\0'), "unable to allocate string");
-
-        frame = get_free_frame(MEM_FRAME_SIZE-1);
-        ASSERT(frame != NULL, "frame should not be available");
-
-        return ret;
-}
-#endif
 
 #ifdef ARCH_X86
 static int multiboot2_magic_1()
@@ -79,8 +53,8 @@ void run_tests()
         run(multiboot2_magic_1);
 #endif
 
-#ifdef CONFIG_MM_FF
-        run(ff_mm_1);
+#if ARCH_X86 && CONFIG_MM_FF
+        TEST(ff_mm_1);
 #endif
 
         printf("Tests run, %d / %d passed\n", tests_passed, tests_passed+tests_failed);
