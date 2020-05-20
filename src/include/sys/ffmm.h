033d20d16fefabc07dfddb226081a22707424730
Anuradha Weeraman <aweeraman@gmail.com>
2020-05-19 21:32:09 -0400

    New FF mm implementation

--- src/include/sys/ffmm.h
+++ src/include/sys/ffmm.h
@@ -10,15 +10,14 @@
 #include <stddef.h>
 
 #define FF_MAGIC          0x3e30
-#define FRAME_UNALLOCATED 1
-#define FRAME_AVAILABLE   2
-#define FRAME_INUSE       4
+#define FRAME_AVAILABLE   0
+#define FRAME_INUSE       1
 #define FRAME_BLOCK_COUNT 1024
 #define FRAME_BLOCK_SIZE  512
 
 struct ff_mm_frame {
-        uint16_t flags;
-        uint16_t addr;
+        uint8_t  flags;
+        uint32_t addr;
 };
 
 struct ff_mm_superblock {
@@ -27,11 +26,12 @@ struct ff_mm_superblock {
         struct ff_mm_superblock *next_super_block;
         uint32_t block_count;
         struct ff_mm_frame blocks[FRAME_BLOCK_COUNT];
-} __attribute__((packed));
+};
 
 typedef struct ff_mm_superblock ff_mm_superblock_t;
 
 void  init_ff_mm();
-void* get_free_frame(size_t size);
+void* get_available_frame(size_t size);
+void  free_frame(uint32_t *addr);
 
 #endif
--- src/sys/ffmm.c
+++ src/sys/ffmm.c
@@ -8,14 +8,44 @@
 #include <lib/string.h>
 
 #define MEM_START_ADDR 0x300000 // initialize mm at the 3MB mark
-#define MEM_FRAME_SIZE 512      // size of each frame of memory
-#define MEM_END_ADDR   0x400000
+#define MEM_END_ADDR   0x380000
 
 static ff_mm_superblock_t *superblock;
 
-void* get_free_frame(size_t size)
+void free_frame(uint32_t *addr)
+{
+        ff_mm_superblock_t *sb;
+        sb = superblock;
+
+        do {
+                for (uint32_t i = 0; i < sb->block_count; i++) {
+                        if (sb->blocks[i].addr == (uint32_t) addr) {
+                                sb->blocks[i].flags = FRAME_AVAILABLE;
+                        }
+                }
+
+                sb = sb->next_super_block;
+        } while (sb != NULL);
+}
+
+void* get_available_frame(size_t size)
 {
         void* ptr = NULL;
+        ff_mm_superblock_t *sb;
+
+        sb = superblock;
+
+        do {
+                for (uint32_t i = 0; i < sb->block_count; i++) {
+                        if (sb->blocks[i].flags == FRAME_AVAILABLE &&
+                                        size < FRAME_BLOCK_SIZE) {
+                                sb->blocks[i].flags = FRAME_INUSE;
+                                return (void *) sb->blocks[i].addr;
+                        }
+                }
+
+                sb = sb->next_super_block;
+        } while (sb != NULL);
 
         return ptr;
 }
@@ -47,29 +77,35 @@ static void create_superblock(uint32_t root_block, uint32_t start_addr, uint32_t
         blocks = available_memory / FRAME_BLOCK_SIZE;
 
         sb->block_count      = blocks;
+
+        for (uint32_t i = 0; i < sb->block_count; i++) {
+                sb->blocks[i].addr = ((uint32_t) sb->start_addr) + (FRAME_BLOCK_SIZE * i);
+        }
 }
 
 static void print_superblocks(uint32_t root_block)
 {
+        int count = 0;
+
         ff_mm_superblock_t *sb;
 
         sb = (ff_mm_superblock_t *) (uint32_t) root_block;
 
         do {
-                printk("  start: 0x%x, blocks: %d, block_size: %dB\n",
+                printk("  %d: start: 0x%x, blocks: %d, block_size: %dB\n",
+                                count++,
                                 sb->start_addr,
                                 sb->block_count,
                                 FRAME_BLOCK_SIZE);
                 sb = sb->next_super_block;
         } while (sb != NULL);
-
-
 }
 
 void init_ff_mm()
 {
         printk("Initializing FirstFit memory manager at 0x%x\n", MEM_START_ADDR);
         create_superblock(MEM_START_ADDR, MEM_START_ADDR, MEM_END_ADDR);
-        create_superblock(MEM_START_ADDR, 0x500000, 0x550000);
+        create_superblock(MEM_START_ADDR, 0x400000, 0x480000);
+        create_superblock(MEM_START_ADDR, 0x500000, 0x580000);
         print_superblocks(MEM_START_ADDR);
 }
