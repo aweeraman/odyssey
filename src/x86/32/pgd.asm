58e3cdfb508d9200c3dd5f03c9298317dce4436a
Anuradha Weeraman <aweeraman@gmail.com>
2020-06-01 22:02:01 -0400

    nasm impl for paging, in progress

---
+++ src/x86/32/pgd.asm
@@ -0,0 +1,13 @@
+; SPDX-FileCopyrightText: 2020 Anuradha Weeraman <anuradha@weeraman.com>
+; SPDX-License-Identifier: GPL-3.0-or-later
+
+global enable_paging
+global init_page_directory
+
+enable_paging:
+        ; TODO
+        ret
+
+init_page_directory:
+        ; TODO
+        ret
