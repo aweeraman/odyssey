312eb4fb3ddfd3a1c502162446cd83837725d9cd
Anuradha Weeraman <aweeraman@gmail.com>
2020-06-08 19:14:17 -0400

    More paging

--- src/include/x86/32/paging.h
+++ src/include/x86/32/paging.h
@@ -8,29 +8,23 @@
 
 #include <stdint.h>
 
-typedef struct page_dir_entry {
-        uint32_t present         : 1;  /* must be 1 to reference a page table */
-        uint32_t rw              : 1;  /* if 0, writes not allowed to the 4MB region controlled by this entry */
-        uint32_t user            : 1;  /* if 0, user mode accesses not allowed to the 4MB region */
-        uint32_t accessed        : 1;  /* indicates whether this entry has been used for access */
-        uint32_t ignored         : 1;  /* ignored */
-        uint32_t reserved        : 7;  /* ignored */
-        uint32_t page_table_addr : 20; /* physical address of 4KB aligned page table */
-}__attribute__((packed)) page_dir_entry_t;
-
-typedef struct page_table_entry {
+typedef struct page_entry {
         uint32_t present         : 1;  /* must be 1 to map to a page */
         uint32_t rw              : 1;  /* if 0, writes not allowed */
         uint32_t user            : 1;  /* if 0, user mode access not allowed */
         uint32_t accessed        : 1;  /* indicates whether page has been accessed */
         uint32_t dirty           : 1;  /* indicates if page has been written to */
         uint32_t reserved        : 7;  /* ignored */
-        uint32_t page_addr       : 20; /* physical address of 4KB page */
-}__attribute__((packed)) page_table_entry_t;
+        uint32_t addr            : 20; /* physical address of 4KB page */
+}__attribute__((packed)) page_entry_t;
+
+typedef struct page_dir_entry {
+        page_entry_t tables[1024];
+        page_entry_t directory[1024];
+}__attribute__((packed)) page_dir_entry_t;
 
 void init_paging         ();
 void init_page_directory (uint32_t *addr);
 void enable_paging       ();
-
 
 #endif
--- src/x86/32/paging.c
+++ src/x86/32/paging.c
@@ -11,14 +11,7 @@
 #define PAGE_DIR_ENTRIES   1024
 #define PAGE_TABLE_ENTRIES 1024
 
-static page_dir_entry_t   page_dir[PAGE_DIR_ENTRIES] \
-                          __attribute__((aligned(PAGE_ALIGNMENT)));
-
-static uint32_t pagedir[PAGE_DIR_ENTRIES] \
-                          __attribute__((aligned(PAGE_ALIGNMENT)));
-
-static page_table_entry_t root_page_table[PAGE_TABLE_ENTRIES] \
-                          __attribute__((aligned(PAGE_ALIGNMENT)));
+static page_dir_entry_t page_dir __attribute__((aligned(PAGE_ALIGNMENT)));
 
 void init_paging()
 {
@@ -27,32 +20,31 @@ void init_paging()
         uint32_t end_phys_addr     = 0x400000;
 
         printk("Initializing paging\n");
-        printk("  Page directory at 0x%x [%d]\n", &page_dir, PAGE_DIR_ENTRIES);
-        printk("  Page table at 0x%x [%d]\n", &root_page_table, PAGE_TABLE_ENTRIES);
+        printk("  Page directory at 0x%x\n", &page_dir);
 
-        memset(&page_dir, 0, PAGE_DIR_ENTRIES);
-        memset(&root_page_table, 0, PAGE_TABLE_ENTRIES);
+        memset(&page_dir, 0, sizeof(page_dir_entry_t));
 
         while (current_phys_addr < end_phys_addr) {
                 uint32_t page_table_idx = current_phys_addr >> 22;
                 uint32_t page_idx = (current_phys_addr >> 12) & 0x3ff;
 
-                root_page_table[page_idx].page_addr = current_phys_addr / 0x1000;
-                root_page_table[page_idx].present   = 1;
-                root_page_table[page_idx].rw        = 1;
-                root_page_table[page_idx].user      = 0;
+                page_dir.tables[page_idx].addr      = current_phys_addr >> 12;
+                page_dir.tables[page_idx].present   = 1;
+                page_dir.tables[page_idx].rw        = 1;
+                page_dir.tables[page_idx].user      = 0;
 
                 current_phys_addr += 0x1000;
 
-                page_dir[page_table_idx].page_table_addr = (uint32_t) root_page_table >> 4;
-                page_dir[page_table_idx].present         = 1;
-                page_dir[page_table_idx].rw              = 1;
-                page_dir[page_table_idx].user            = 0;
+                // TODO clean this up
+                page_dir.directory[page_table_idx].addr      = (uint32_t) page_dir.tables >> 12;
+                page_dir.directory[page_table_idx].present   = 1;
+                page_dir.directory[page_table_idx].rw        = 1;
+                page_dir.directory[page_table_idx].user      = 0;
         }
 
-        peek(&page_dir, 1);
-        peek(&root_page_table[0x3ff], 1);
+        peek(&page_dir.directory, 1);
+        peek(&page_dir.tables[0x234], 1);
 
-        init_page_directory((uint32_t *) page_dir);
-        enable_paging();
+        //init_page_directory((uint32_t *) page_dir.directory);
+        //enable_paging();
 }
