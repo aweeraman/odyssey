9385f95a26df0d9578d984fe1b9de4aea455c378
Anuradha Weeraman <aweeraman@gmail.com>
2020-06-08 16:28:29 -0400

    Pretty print binary

--- src/include/lib/mm.h
+++ src/include/lib/mm.h
@@ -13,6 +13,40 @@
 
 union mem_ptr {
         struct {
+                uint8_t b0 : 1;
+                uint8_t b1 : 1;
+                uint8_t b2 : 1;
+                uint8_t b3 : 1;
+                uint8_t b4 : 1;
+                uint8_t b5 : 1;
+                uint8_t b6 : 1;
+                uint8_t b7 : 1;
+                uint8_t b8 : 1;
+                uint8_t b9 : 1;
+                uint8_t b10: 1;
+                uint8_t b11: 1;
+                uint8_t b12: 1;
+                uint8_t b13: 1;
+                uint8_t b14: 1;
+                uint8_t b15: 1;
+                uint8_t b16: 1;
+                uint8_t b17: 1;
+                uint8_t b18: 1;
+                uint8_t b19: 1;
+                uint8_t b20: 1;
+                uint8_t b21: 1;
+                uint8_t b22: 1;
+                uint8_t b23: 1;
+                uint8_t b24: 1;
+                uint8_t b25: 1;
+                uint8_t b26: 1;
+                uint8_t b27: 1;
+                uint8_t b28: 1;
+                uint8_t b29: 1;
+                uint8_t b30: 1;
+                uint8_t b31: 1;
+        } bit;
+        struct {
                 uint8_t n1: 4;
                 uint8_t n2: 4;
                 uint8_t n3: 4;
