fe89a5cff65be6ca1c9c8339e34f425b01dd9442
Anuradha Weeraman <aweeraman@gmail.com>
2020-06-08 13:26:04 -0400

    Not triple faulting anymore due to paging, getting page faults now

--- src/include/x86/32/paging.h
+++ src/include/x86/32/paging.h
@@ -12,12 +12,9 @@ typedef struct page_dir_entry {
         uint32_t present         : 1;  /* must be 1 to reference a page table */
         uint32_t rw              : 1;  /* if 0, writes not allowed to the 4MB region controlled by this entry */
         uint32_t user            : 1;  /* if 0, user mode accesses not allowed to the 4MB region */
-        uint32_t write_through   : 1;  /* page level write through cache */
-        uint32_t disable_cache   : 1;  /* page level cache disable */
         uint32_t accessed        : 1;  /* indicates whether this entry has been used for access */
         uint32_t ignored         : 1;  /* ignored */
-        uint32_t page_size       : 1;  /* if CR4.PSE = 1, must be zero, otherwise ignored */
-        uint32_t reserved        : 4;  /* ignored */
+        uint32_t reserved        : 7;  /* ignored */
         uint32_t page_table_addr : 20; /* physical address of 4KB aligned page table */
 }__attribute__((packed)) page_dir_entry_t;
 
@@ -25,16 +22,15 @@ typedef struct page_table_entry {
         uint32_t present         : 1;  /* must be 1 to map to a page */
         uint32_t rw              : 1;  /* if 0, writes not allowed */
         uint32_t user            : 1;  /* if 0, user mode access not allowed */
-        uint32_t write_through   : 1;  /* page level write through */
-        uint32_t disable_cache   : 1;  /* page level cache disable */
         uint32_t accessed        : 1;  /* indicates whether page has been accessed */
         uint32_t dirty           : 1;  /* indicates if page has been written to */
-        uint32_t memory_type     : 1;  /* zero unless PAT is supported */
-        uint32_t global          : 1;  /* ignored unless CR4.PGE = 1 */
-        uint32_t reserved        : 3;  /* ignored */
+        uint32_t reserved        : 7;  /* ignored */
         uint32_t page_addr       : 20; /* physical address of 4KB page */
 }__attribute__((packed)) page_table_entry_t;
 
-void init_paging();
+void init_paging         ();
+void init_page_directory (uint32_t *addr);
+void enable_paging       ();
+
 
 #endif
--- src/x86/32/paging.c
+++ src/x86/32/paging.c
@@ -11,37 +11,41 @@
 #define PAGE_DIR_ENTRIES   1024
 #define PAGE_TABLE_ENTRIES 1024
 
-extern void init_page_directory(uint32_t *addr);
-extern void enable_paging();
+static page_dir_entry_t   page_dir[PAGE_DIR_ENTRIES] \
+                          __attribute__((aligned(PAGE_ALIGNMENT)));
 
-page_dir_entry_t   page_dir[PAGE_DIR_ENTRIES] \
-                   __attribute__((aligned(PAGE_ALIGNMENT)));
-
-page_table_entry_t root_page_table[PAGE_TABLE_ENTRIES] \
-                   __attribute__((aligned(PAGE_ALIGNMENT)));
+static page_table_entry_t root_page_table[PAGE_TABLE_ENTRIES] \
+                          __attribute__((aligned(PAGE_ALIGNMENT)));
 
 void init_paging()
 {
         printk("Initializing paging\n");
+        printk("  Page directory at 0x%x [%d]\n", &page_dir, PAGE_DIR_ENTRIES);
+        printk("  Page table at 0x%x [%d]\n", &root_page_table, PAGE_TABLE_ENTRIES);
 
         memset(&page_dir, 0, PAGE_DIR_ENTRIES);
         memset(&root_page_table, 0, PAGE_TABLE_ENTRIES);
+
+        /* Initialize the page directory entries */
+        for (int i = 0; i < PAGE_DIR_ENTRIES; i++) {
+                page_dir[i].present = 0x0;
+                page_dir[i].rw      = 0x1;
+                page_dir[i].user    = 0x0;
+        }
 
         /* Mapping first 4MB of memory */
         for (int i = 0; i < PAGE_TABLE_ENTRIES; i++) {
                 root_page_table[i].present   = 1;
                 root_page_table[i].rw        = 1;
-                root_page_table[i].page_addr = i * 0x1000;
-        }
-
-        /* Initialize the page directory entries */
-        for (int i = 0; i < PAGE_DIR_ENTRIES; i++) {
-                page_dir[i].rw = 1;
+                root_page_table[i].user      = 0;
+                root_page_table[i].page_addr = (i * 0x1000) >> 12;
         }
 
         /* Set the mapping for the first page table entry */
-        page_dir[0].page_table_addr = (uint32_t) root_page_table;
+        page_dir[0].page_table_addr = ((uint32_t) &root_page_table) >> 12;
         page_dir[0].present         = 1;
+        page_dir[0].rw              = 1;
+        page_dir[0].user            = 0;
 
         //init_page_directory((uint32_t *) &page_dir);
         //enable_paging();
