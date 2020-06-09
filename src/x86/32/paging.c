cb8251c06e4a961986e03e66156eb69feb975d55
Anuradha Weeraman <aweeraman@gmail.com>
2020-06-08 21:14:03 -0400

    Refactored paging

--- src/x86/32/paging.c
+++ src/x86/32/paging.c
@@ -11,40 +11,39 @@
 #define PAGE_DIR_ENTRIES   1024
 #define PAGE_TABLE_ENTRIES 1024
 
-static page_dir_entry_t kernel_page_dir __attribute__((aligned(PAGE_ALIGNMENT)));
+static page_dir_entry_t kernel_pg_dir __attribute__((aligned(PAGE_ALIGNMENT)));
 
 static void map_memory(uint32_t phys_start, uint32_t phys_end)
 {
         uint32_t phys_cur = phys_start;
 
         while (phys_cur < phys_end) {
-                uint32_t page_table_idx = phys_cur >> 22;
+                uint32_t table_idx = phys_cur >> 22;
                 uint32_t page_idx = (phys_cur >> 12) & 0x3ff;
 
-                kernel_page_dir.tables[page_idx].addr      = phys_cur >> 12;
-                kernel_page_dir.tables[page_idx].present   = 1;
-                kernel_page_dir.tables[page_idx].rw        = 1;
-                kernel_page_dir.tables[page_idx].user      = 0;
+                kernel_pg_dir.tables[page_idx].addr      = phys_cur >> 12;
+                kernel_pg_dir.tables[page_idx].present   = 1;
+                kernel_pg_dir.tables[page_idx].rw        = 1;
+                kernel_pg_dir.tables[page_idx].user      = 0;
 
                 phys_cur += 0x1000;
 
                 // TODO clean this up
-                kernel_page_dir.directory[page_table_idx].addr      = (uint32_t) kernel_page_dir.tables >> 12;
-                kernel_page_dir.directory[page_table_idx].present   = 1;
-                kernel_page_dir.directory[page_table_idx].rw        = 1;
-                kernel_page_dir.directory[page_table_idx].user      = 0;
+                kernel_pg_dir.directory[table_idx].addr      = (uint32_t) kernel_pg_dir.tables >> 12;
+                kernel_pg_dir.directory[table_idx].present   = 1;
+                kernel_pg_dir.directory[table_idx].rw        = 1;
+                kernel_pg_dir.directory[table_idx].user      = 0;
         }
 
-        peek(&kernel_page_dir.directory, 1);
-        peek(&kernel_page_dir.tables[0x234], 1);
+        peek(&kernel_pg_dir.directory, 1);
+        peek(&kernel_pg_dir.tables[0x234], 1);
 }
 
 void init_paging()
 {
-        printk("Initializing paging\n");
-        printk("  Page directory at 0x%x\n", &kernel_page_dir);
-        memset(&kernel_page_dir, 0, sizeof(page_dir_entry_t));
+        printk("Initializing paging, kernel page directory at 0x%x\n", &kernel_pg_dir);
+        memset(&kernel_pg_dir, 0, sizeof(page_dir_entry_t));
         map_memory(0x0, 0x450000);
-        switch_page_directory((uint32_t *) kernel_page_dir.directory);
+        switch_page_directory((uint32_t *) kernel_pg_dir.directory);
         enable_paging();
 }
