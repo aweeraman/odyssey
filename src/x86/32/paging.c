2cc7a72188ac06cf75547c723c57952f5defa80b
Anuradha Weeraman <aweeraman@gmail.com>
2020-06-08 21:01:00 -0400

    Refactored

--- src/x86/32/paging.c
+++ src/x86/32/paging.c
@@ -11,7 +11,7 @@
 #define PAGE_DIR_ENTRIES   1024
 #define PAGE_TABLE_ENTRIES 1024
 
-static page_dir_entry_t page_dir __attribute__((aligned(PAGE_ALIGNMENT)));
+static page_dir_entry_t kernel_page_dir __attribute__((aligned(PAGE_ALIGNMENT)));
 
 void init_paging()
 {
@@ -20,31 +20,31 @@ void init_paging()
         uint32_t end_phys_addr     = 0x450000;
 
         printk("Initializing paging\n");
-        printk("  Page directory at 0x%x\n", &page_dir);
+        printk("  Page directory at 0x%x\n", &kernel_page_dir);
 
-        memset(&page_dir, 0, sizeof(page_dir_entry_t));
+        memset(&kernel_page_dir, 0, sizeof(page_dir_entry_t));
 
         while (current_phys_addr < end_phys_addr) {
                 uint32_t page_table_idx = current_phys_addr >> 22;
                 uint32_t page_idx = (current_phys_addr >> 12) & 0x3ff;
 
-                page_dir.tables[page_idx].addr      = current_phys_addr >> 12;
-                page_dir.tables[page_idx].present   = 1;
-                page_dir.tables[page_idx].rw        = 1;
-                page_dir.tables[page_idx].user      = 0;
+                kernel_page_dir.tables[page_idx].addr      = current_phys_addr >> 12;
+                kernel_page_dir.tables[page_idx].present   = 1;
+                kernel_page_dir.tables[page_idx].rw        = 1;
+                kernel_page_dir.tables[page_idx].user      = 0;
 
                 current_phys_addr += 0x1000;
 
                 // TODO clean this up
-                page_dir.directory[page_table_idx].addr      = (uint32_t) page_dir.tables >> 12;
-                page_dir.directory[page_table_idx].present   = 1;
-                page_dir.directory[page_table_idx].rw        = 1;
-                page_dir.directory[page_table_idx].user      = 0;
+                kernel_page_dir.directory[page_table_idx].addr      = (uint32_t) kernel_page_dir.tables >> 12;
+                kernel_page_dir.directory[page_table_idx].present   = 1;
+                kernel_page_dir.directory[page_table_idx].rw        = 1;
+                kernel_page_dir.directory[page_table_idx].user      = 0;
         }
 
-        //peek(&page_dir.directory, 1);
-        //peek(&page_dir.tables[0x234], 1);
+        peek(&kernel_page_dir.directory, 1);
+        peek(&kernel_page_dir.tables[0x234], 1);
 
-        //init_page_directory((uint32_t *) page_dir.directory);
-        //enable_paging();
+        init_page_directory((uint32_t *) kernel_page_dir.directory);
+        enable_paging();
 }
