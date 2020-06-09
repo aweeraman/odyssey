39d13a88ba7abe4ec87e7841fbf8d9d52e75cf5f
Anuradha Weeraman <aweeraman@gmail.com>
2020-06-08 21:10:26 -0400

    Refactored paging

--- src/x86/32/paging.c
+++ src/x86/32/paging.c
@@ -13,22 +13,20 @@
 
 static page_dir_entry_t kernel_page_dir __attribute__((aligned(PAGE_ALIGNMENT)));
 
-static void map_memory()
+static void map_memory(uint32_t phys_start, uint32_t phys_end)
 {
-        uint32_t current_phys_addr = 0x0;
-        uint32_t start_phys_addr   = 0x0;
-        uint32_t end_phys_addr     = 0x450000;
+        uint32_t phys_cur = phys_start;
 
-        while (current_phys_addr < end_phys_addr) {
-                uint32_t page_table_idx = current_phys_addr >> 22;
-                uint32_t page_idx = (current_phys_addr >> 12) & 0x3ff;
+        while (phys_cur < phys_end) {
+                uint32_t page_table_idx = phys_cur >> 22;
+                uint32_t page_idx = (phys_cur >> 12) & 0x3ff;
 
-                kernel_page_dir.tables[page_idx].addr      = current_phys_addr >> 12;
+                kernel_page_dir.tables[page_idx].addr      = phys_cur >> 12;
                 kernel_page_dir.tables[page_idx].present   = 1;
                 kernel_page_dir.tables[page_idx].rw        = 1;
                 kernel_page_dir.tables[page_idx].user      = 0;
 
-                current_phys_addr += 0x1000;
+                phys_cur += 0x1000;
 
                 // TODO clean this up
                 kernel_page_dir.directory[page_table_idx].addr      = (uint32_t) kernel_page_dir.tables >> 12;
@@ -46,7 +44,7 @@ void init_paging()
         printk("Initializing paging\n");
         printk("  Page directory at 0x%x\n", &kernel_page_dir);
         memset(&kernel_page_dir, 0, sizeof(page_dir_entry_t));
-        map_memory();
+        map_memory(0x0, 0x450000);
         switch_page_directory((uint32_t *) kernel_page_dir.directory);
         enable_paging();
 }
