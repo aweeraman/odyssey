782c512a160ad69cb5944b55e22afa4b6cd64ed9
Anuradha Weeraman <aweeraman@gmail.com>
2020-07-30 19:10:51 -0400

    Fixed issue with jpeg-to-c.sh

--- tools/ppm-to-array/jpeg-to-c.sh
+++ tools/ppm-to-array/jpeg-to-c.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+# SPDX-FileCopyrightText: 2020 Anuradha Weeraman <anuradha@weeraman.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 CURDIR=$(dirname $(readlink -f "$0"))
-JPEG_FILE=$1
-HEADER_FILE=$2
 
 if [ "$#" -lt "2" ]; then
 	echo "Usage: $0 jpeg-file header-file.h"
 	exit 0
 fi
+
+JPEG_FILE=$1
+HEADER_FILE=$2
 
 which jpegtopnm > /dev/null
 if [ $? -ne 0 ]; then
@@ -20,8 +23,8 @@ if [ ! -e ${CURDIR}/ppm-to-array ]; then
 	make -C ${CURDIR}
 fi
 
-if [ ! -e "${HEADER_FILE}" ]; then
-	echo "$HEADER_FILE not found!"
+if [ ! -e "${JPEG_FILE}" ]; then
+	echo "$JPEG_FILE not found!"
 	exit 1
 fi
 
